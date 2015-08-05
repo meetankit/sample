b01693618952139c8140384b84ce5a6b1feb2436
Ankit Agarwal (SaaS) <Ankit.Agarwal2@citrix.com>
2015-08-05 16:35:19 +0530

    refactor

--- src/main/java/com/citrix/g2w/microservice/ApiProxyApplication.java
+++ src/main/java/com/citrix/g2w/microservice/ApiProxyApplication.java
@@ -1,8 +1,10 @@
 package com.citrix.g2w.microservice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +13,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @EnableZuulProxy
 @SpringBootApplication
 @RestController
-@EnableCircuitBreaker
 public class ApiProxyApplication {
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
